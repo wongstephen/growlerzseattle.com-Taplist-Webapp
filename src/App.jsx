5cbc35010fbf908228a7a0ed51007427090ba448
Stephen Wong <wongstephenky@gmail.com>
2023-02-13 17:35:43 -0800

    toots

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,23 @@ import Home from "./Home";
 const App = () => {
   const [showHome, setShowHome] = useState(false);
 
-  // useEffect(() => {
-  //   if (showHome) {
-  //     setTimeout(() => {
-  //       setShowHome((prev) => {
-  //         return false;
-  //       });
-  //     }, 10000);
-  //     // 10 min is 600000 ms
-  //   } else {
-  //     setTimeout(() => {
-  //       setShowHome((prev) => {
-  //         return true;
-  //       });
-  //     }, 5000);
-  //     // 1 minutes is 60000 ms
-  //   }
-  // }, [setShowHome]);
+  useEffect(() => {
+    if (showHome) {
+      setTimeout(() => {
+        setShowHome((prev) => {
+          return false;
+        });
+      }, 10000);
+      // 10 min is 600000 ms
+    } else {
+      setTimeout(() => {
+        setShowHome((prev) => {
+          return true;
+        });
+      }, 5000);
+      // 1 minutes is 60000 ms
+    }
+  }, [setShowHome]);
 
   return <div className="app">{showHome ? <Home /> : <Events />}</div>;
 };
